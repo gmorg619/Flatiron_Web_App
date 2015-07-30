48ea1bafeac531a64e9011dcefce320316fa7e9e
Greg Morgan <gmorg619@gmail.com>
2015-07-30 17:03:10 +0000

    Trying to Commit

--- application_controller.rb
+++ application_controller.rb
@@ -20,11 +20,11 @@ class MyApp < Sinatra::Base
   #POST sends data to the server
   post '/celeb_page' do
     @celeb_hash = {
-  "Taylor Swift" => {:name    => "Taylor Swift",
+  "Taylor Swift" => {:name    => "<h1>Taylor Swift</h1>",
               :Twitter => "<blockquote class='twitter-tweet' lang='en'><p lang='en' dir='ltr'>&quot;Your little hand&#39;s wrapped around my finger and it&#39;s so quiet in the world tonight...&quot; <a href='http://t.co/nGaWkKHmJk'>pic.twitter.com/nGaWkKHmJk</a></p>&mdash; Taylor Swift (@taylorswift13) <a href='https://twitter.com/taylorswift13/status/626153646724550656'>July 28, 2015</a></blockquote><script async src='//platform.twitter.com/widgets.js' charset='utf-8'></script>",
               :website => "http://taylorswift.com/",
               :youtube => "<iframe width='560' height='315' src='https://www.youtube.com/embed/QcIy9NiNbmo' frameborder='0' allowfullscreen></iframe>"},
-  "Drake"  => {:name    => "Drake",
+  "Drake"  => {:name    => "<h1>Drake</h1>",
               :Twitter => "<blockquote class='twitter-tweet' lang='en'><p lang='und' dir='ltr'><a href='https://t.co/jWMfEPxBqM'>https://t.co/jWMfEPxBqM</a></p>&mdash; Drizzy (@Drake) <a href='https://twitter.com/Drake/status/626309606810128384'>July 29, 2015</a></blockquote><script async src='//platform.twitter.com/widgets.js' charset='utf-8'></script>",
               :website => "http://www.drakeofficial.com/",
               :youtube => "<iframe width='420' height='315' src='https://www.youtube.com/embed/TjaMxaH4kjg' frameborder='0' allowfullscreen></iframe>"}
