054f87e95254c1fcead6c0714732f84e2339044c
Greg Morgan <gmorg619@gmail.com>
2015-07-30 17:54:22 +0000

    added to hash

--- application_controller.rb
+++ application_controller.rb
@@ -21,13 +21,19 @@ class MyApp < Sinatra::Base
   post '/celeb_page' do
     @celeb_hash = {
   "Taylor Swift" => {:name    => "<h1>Taylor Swift</h1>",
-              :Twitter => "<blockquote class='twitter-tweet' lang='en'><p lang='en' dir='ltr'>&quot;Your little hand&#39;s wrapped around my finger and it&#39;s so quiet in the world tonight...&quot; <a href='http://t.co/nGaWkKHmJk'>pic.twitter.com/nGaWkKHmJk</a></p>&mdash; Taylor Swift (@taylorswift13) <a href='https://twitter.com/taylorswift13/status/626153646724550656'>July 28, 2015</a></blockquote><script async src='//platform.twitter.com/widgets.js' charset='utf-8'></script>",
+              :picture => "<img id='tswift' src='http://static.guim.co.uk/sys-images/Guardian/Pix/pictures/2014/8/21/1408618840671/18839f21-608b-4acd-a2aa-a944db3e15e7-2060x1236.jpeg'>",
+              :Twitter => "<a class='twitter-timeline' href='https://twitter.com/taylorswift13' data-widget-id='626771222114775040'>Tweets by @taylorswift13</a>
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document,'script','twitter-wjs');</script>",
               :website => "http://taylorswift.com/",
               :youtube => "<iframe width='560' height='315' src='https://www.youtube.com/embed/QcIy9NiNbmo' frameborder='0' allowfullscreen></iframe>"},
   "Drake"  => {:name    => "<h1>Drake</h1>",
-              :Twitter => "<blockquote class='twitter-tweet' lang='en'><p lang='und' dir='ltr'><a href='https://t.co/jWMfEPxBqM'>https://t.co/jWMfEPxBqM</a></p>&mdash; Drizzy (@Drake) <a href='https://twitter.com/Drake/status/626309606810128384'>July 29, 2015</a></blockquote><script async src='//platform.twitter.com/widgets.js' charset='utf-8'></script>",
+              :picture => "<img id='celeb_pic' src='http://djmag.ca/wp-content/uploads/2015/02/drake-remixes.jpg' style=verticaalign='middle'>",
+              :Twitter => "<div><a class='twitter-timeline' href='https://twitter.com/Drake' data-widget-id='626762999680880640' width>Tweets by @Drake</a>
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document,'script','twitter-wjs');</script><script async src='//platform.twitter.com/widgets.js' charset='utf-8'></script></div>",
               :website => "http://www.drakeofficial.com/",
-              :youtube => "<iframe width='420' height='315' src='https://www.youtube.com/embed/TjaMxaH4kjg' frameborder='0' allowfullscreen></iframe>"}
+              :soundloud => "<iframe width='75%' height='450' scrolling='no' frameborder='no' src='https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/users/1078461&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true'></iframe>",
+              :youtube => "<iframe width='420' height='315' src='https://www.youtube.com/embed/TjaMxaH4kjg' frameborder='0' allowfullscreen></iframe>",
+              :spotify => "<iframe id='spotify' src='https://embed.spotify.com/?uri=spotify%3Aartist%3A3TVXtAsR1Inumwj472S9r4' width='300' height='380' frameborder='0' allowtransparency='true'></iframe>"}
 
 }
     person = params[:person]
