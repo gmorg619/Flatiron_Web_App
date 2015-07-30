b5c06bb37507e8fa1116cfac3cf84907e474ad7c
Greg Morgan <gmorg619@gmail.com>
2015-07-30 16:53:15 +0000

    fixed hash methods

--- application_controller.rb
+++ application_controller.rb
@@ -8,15 +8,33 @@ class MyApp < Sinatra::Base
   get '/' do
     erb :index
   end
+  
+   get '/taylor_swift' do
+    erb :taylor_swift
+  end
+  get '/drake' do
+    erb :drake
+  end
 
 
   #POST sends data to the server
   post '/celeb_page' do
-    x = params[:person]
-    @celeb_name = print_celeb_name(celeb_hash(x))
-    @print_celeb_info = print_celeb_info(celeb_hash[x])
-    puts params[:person]
-    erb :celeb_page
+    @celeb_hash = {
+  "Taylor Swift" => {:name    => "Taylor Swift",
+              :Twitter => "<blockquote class='twitter-tweet' lang='en'><p lang='en' dir='ltr'>&quot;Your little hand&#39;s wrapped around my finger and it&#39;s so quiet in the world tonight...&quot; <a href='http://t.co/nGaWkKHmJk'>pic.twitter.com/nGaWkKHmJk</a></p>&mdash; Taylor Swift (@taylorswift13) <a href='https://twitter.com/taylorswift13/status/626153646724550656'>July 28, 2015</a></blockquote><script async src='//platform.twitter.com/widgets.js' charset='utf-8'></script>",
+              :website => "http://taylorswift.com/",
+              :youtube => "<iframe width='560' height='315' src='https://www.youtube.com/embed/QcIy9NiNbmo' frameborder='0' allowfullscreen></iframe>"},
+  "Drake"  => {:name    => "Drake",
+              :Twitter => "<blockquote class='twitter-tweet' lang='en'><p lang='und' dir='ltr'><a href='https://t.co/jWMfEPxBqM'>https://t.co/jWMfEPxBqM</a></p>&mdash; Drizzy (@Drake) <a href='https://twitter.com/Drake/status/626309606810128384'>July 29, 2015</a></blockquote><script async src='//platform.twitter.com/widgets.js' charset='utf-8'></script>",
+              :website => "http://www.drakeofficial.com/",
+              :youtube => "<iframe width='420' height='315' src='https://www.youtube.com/embed/TjaMxaH4kjg' frameborder='0' allowfullscreen></iframe>"}
+
+}
+    person = params[:person]
+    if @celeb_hash.has_key?(person)
+      print_celeb_name(@celeb_hash[person])
+      print_celeb_info(@celeb_hash[person])
+    end
   end
 
 end
--- models/celeb_smash.rb
+++ models/celeb_smash.rb
@@ -1,22 +1,38 @@
-celeb_hash = {
-  "Taylor Swift" => {:name    => "Taylor Swift",
-              :Twitter => "<blockquote class='twitter-tweet' lang='en'><p lang='en' dir='ltr'>&quot;Your little hand&#39;s wrapped around my finger and it&#39;s so quiet in the world tonight...&quot; <a href='http://t.co/nGaWkKHmJk'>pic.twitter.com/nGaWkKHmJk</a></p>&mdash; Taylor Swift (@taylorswift13) <a href='https://twitter.com/taylorswift13/status/626153646724550656'>July 28, 2015</a></blockquote><script async src='//platform.twitter.com/widgets.js' charset='utf-8'></script>",
-              :website => "http://taylorswift.com/",
-              :youtube => "<iframe width='560' height='315' src='https://www.youtube.com/embed/QcIy9NiNbmo' frameborder='0' allowfullscreen></iframe>"},
-  :Drake  => {:name    => "Drake",
-              :Twitter => "<blockquote class='twitter-tweet' lang='en'><p lang='und' dir='ltr'><a href='https://t.co/jWMfEPxBqM'>https://t.co/jWMfEPxBqM</a></p>&mdash; Drizzy (@Drake) <a href='https://twitter.com/Drake/status/626309606810128384'>July 29, 2015</a></blockquote><script async src='//platform.twitter.com/widgets.js' charset='utf-8'></script>",
-              :website => "http://www.drakeofficial.com/",
-              :youtube => "<iframe width='420' height='315' src='https://www.youtube.com/embed/TjaMxaH4kjg' frameborder='0' allowfullscreen></iframe>"}
+# @celeb_hash = {
+#   "Taylor Swift" => {:name    => "Taylor Swift",
+#               :Twitter => "<blockquote class='twitter-tweet' lang='en'><p lang='en' dir='ltr'>&quot;Your little hand&#39;s wrapped around my finger and it&#39;s so quiet in the world tonight...&quot; <a href='http://t.co/nGaWkKHmJk'>pic.twitter.com/nGaWkKHmJk</a></p>&mdash; Taylor Swift (@taylorswift13) <a href='https://twitter.com/taylorswift13/status/626153646724550656'>July 28, 2015</a></blockquote><script async src='//platform.twitter.com/widgets.js' charset='utf-8'></script>",
+#               :website => "http://taylorswift.com/",
+#               :youtube => "<iframe width='560' height='315' src='https://www.youtube.com/embed/QcIy9NiNbmo' frameborder='0' allowfullscreen></iframe>"},
+#   "Drake"  => {:name    => "Drake",
+#               :Twitter => "<blockquote class='twitter-tweet' lang='en'><p lang='und' dir='ltr'><a href='https://t.co/jWMfEPxBqM'>https://t.co/jWMfEPxBqM</a></p>&mdash; Drizzy (@Drake) <a href='https://twitter.com/Drake/status/626309606810128384'>July 29, 2015</a></blockquote><script async src='//platform.twitter.com/widgets.js' charset='utf-8'></script>",
+#               :website => "http://www.drakeofficial.com/",
+#               :youtube => "<iframe width='420' height='315' src='https://www.youtube.com/embed/TjaMxaH4kjg' frameborder='0' allowfullscreen></iframe>"}
 
-}
-
+# }
 
 def print_celeb_name(hash)
-  return hash[:name]
+  if hash.has_key?(:name)
+    return hash[:name]
+  end
 end
 
 def print_celeb_info(hash)
+  celeb_info_array = []
     hash.each_value do |value|
-      return value
+      celeb_info_array << value
     end
+  return celeb_info_array
 end
+
+# params = {"person" => "Taylor Swift"}
+# person = params["person"]
+
+# if @celeb_hash.has_key?(person)
+#    puts print_celeb_name(@celeb_hash[person])
+#    puts print_celeb_info(@celeb_hash[person])
+# end
+
+
+
+
+
